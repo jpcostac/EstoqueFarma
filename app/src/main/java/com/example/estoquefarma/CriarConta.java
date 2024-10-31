17ce2732207bda1f1f929adf7f5ae7cb9cf1a25f
31032019 <jpcostac17@gmail.com>
2024-10-30 23:36:42 -0300

    Improve login validation flow and error handling

--- app/src/main/java/com/example/estoquefarma/CriarConta.java
+++ app/src/main/java/com/example/estoquefarma/CriarConta.java
@@ -1,9 +1,12 @@
 package com.example.estoquefarma;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,18 +16,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CriarConta extends AppCompatActivity {
 
+    private EditText inputEmail, inputPassword;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_criar_conta);
 
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
         Button button = findViewById(R.id.criarConta);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CriarConta.this, TelaInicial.class);
-                startActivity(intent);
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                if(email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(CriarConta.this,"Por favor preeencha todos os campos",Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent intent = new Intent(CriarConta.this, TelaInicial.class);
+                    startActivity(intent);
+                }
             }
         });
     }
--- app/src/main/java/com/example/estoquefarma/MainActivity.java
+++ app/src/main/java/com/example/estoquefarma/MainActivity.java
@@ -4,24 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
         TextView textView = findViewById(R.id.textView3);
         Button button = findViewById(R.id.button);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TelaInicial.class);
-                startActivity(intent);
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                if(email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Por favor preeencha todos os campos",Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent intent = new Intent(MainActivity.this, TelaInicial.class);
+                    startActivity(intent);
+                }
             }
         });
 
